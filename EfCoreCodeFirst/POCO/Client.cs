0e6d914557ca699a728dd95b5949b1438b344b28
tudorsavu <48492437+tudorsavu@users.noreply.github.com>
2020-03-23 11:27:11 +0200

    Add files via upload

---
+++ EfCoreCodeFirst/POCO/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using EfCoreCodeFirst.POCO;
+
+namespace EfCoreCodeFirst
+{
+    public partial class Client
+    {
+        public int ClientId { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        protected Client() { }
+    }
+}
---
+++ EfCoreCodeFirst/POCO/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfCoreCodeFirst.POCO
+{
+    public partial class Order
+    {
+        protected Order() { }
+
+        public Client Client;
+    }
+}
