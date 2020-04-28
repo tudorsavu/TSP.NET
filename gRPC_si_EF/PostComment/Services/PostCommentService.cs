f15e0d923306348c33b8ad347897c7904587fb0a
tudorsavu <48492437+tudorsavu@users.noreply.github.com>
2020-04-28 15:51:08 +0300

    Add files via upload

---
+++ gRPC_si_EF/PostComment/Services/PostCommentService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+
+namespace PostComment
+{
+    public class PostCommentService
+    {
+       
+    }
+}
