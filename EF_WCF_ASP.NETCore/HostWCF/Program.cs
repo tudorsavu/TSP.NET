f48d58ac1d246297469d586f003fe88895933e75
tudorsavu <48492437+tudorsavu@users.noreply.github.com>
2020-05-12 15:28:20 +0300

    Add files via upload

---
+++ EF_WCF_ASP.NETCore/HostWCF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using ObjectWCF;
+using System.ServiceModel.Description;
+namespace HostWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lansare server WCF...");
+            ServiceHost host = new ServiceHost(typeof(PostComment),
+            new Uri("http://localhost:8000/PC"));
+           
+            host.Open();
+            Console.WriteLine("Server in executie. Se asteapta conexiuni...");
+            Console.WriteLine("Apasati Enter pentru a opri serverul!");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
