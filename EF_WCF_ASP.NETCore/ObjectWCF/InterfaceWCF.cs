5a7f7cb0ace00dac40d418b6ba154598246bafb5
tudorsavu <48492437+tudorsavu@users.noreply.github.com>
2020-05-12 15:29:29 +0300

    Add files via upload

---
+++ EF_WCF_ASP.NETCore/ObjectWCF/InterfaceWCF.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using PostComment;
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfacePost
+    {
+        [OperationContract]
+        bool AddPost(Post post);
+        [OperationContract]
+        Post UpdatePost(Post post);
+        [OperationContract]
+        int DeletePost(int id);
+        [OperationContract]
+        Post GetPostById(int id);
+        [OperationContract]
+        List<Post> GetPosts();
+    }
+    [ServiceContract]
+    interface InterfaceComment
+    {
+        [OperationContract]
+        bool AddComment(Comment comment);
+        [OperationContract]
+        Comment UpdateComment(Comment newComment);
+        [OperationContract]
+        Comment GetCommentById(int id);
+    }
+    [ServiceContract]
+    interface IPostComment : InterfacePost, InterfaceComment
+    {
+    }
+}
