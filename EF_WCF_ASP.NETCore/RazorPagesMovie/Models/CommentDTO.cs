ed76bb851bcc6be543e5ce1e8e3423594b90715b
tudorsavu <48492437+tudorsavu@users.noreply.github.com>
2020-05-12 15:33:37 +0300

    Add files via upload

---
+++ EF_WCF_ASP.NETCore/RazorPagesMovie/Models/CommentDTO.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace RazorPagesMovie.Models
+{
+    public class CommentDTO
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public int PostPostId { get; set; }
+        public virtual PostDTO Post { get; set; }
+    }
+}
---
+++ EF_WCF_ASP.NETCore/RazorPagesMovie/Models/PostDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class PostDTO
+    {
+        public PostDTO()    {this.Comments = new HashSet<CommentDTO>();}
+        public int PostId   { get; set; }
+        public string Description   { get; set; }
+        public string Domain    { get; set; }
+
+        [DataType(DataType.Date)]
+        public System.DateTime Date     { get; set; }
+        public virtual ICollection<CommentDTO> Comments    { get; set; }
+    }
+}
